70201b705eecd2748652d12c1ac9c2cd2615d984
jufelka <zedxlab@gmail.com>
2025-11-23 18:11:52 +0100

    Add project files.

---
+++ EnsService.cs
@@ -0,0 +1,25 @@
+﻿using Nethereum.Web3;
+
+internal class EnsService(string jsonRpc)
+{
+    public Web3 Web3 { get; } = new(jsonRpc);
+
+    public async Task<string> GetEnsFromAddress(string ethAddress)
+    {
+        var ensService = Web3.Eth.GetEnsService();
+
+        string ensName = "";
+
+		try
+		{
+			ensName = await ensService.ReverseResolveAsync(ethAddress);
+
+		}
+        catch (Exception)
+        {
+            // ignored
+        }
+
+        return ensName;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+
+AnsiConsole.Write(new FigletText("ENS Reverse Resolver").Color(Color.Gold1));
+
+var rpcUrl = AnsiConsole.Ask<string>("Enter RPC url:");
+var addressesPath = AnsiConsole.Ask<string>("Enter the path to the ETH addresses:");
+
+if (!File.Exists(addressesPath))
+{
+    AnsiConsole.MarkupLine("[red]File not found![/]");
+    return;
+}
+
+var ensService = new EnsService(rpcUrl);
+
+foreach (var address in File.ReadLines(addressesPath))
+{
+    string resolvedEns = await ensService.GetEnsFromAddress(address);
+
+    if (string.IsNullOrEmpty(resolvedEns))
+        continue;
+
+    var line = new[]
+    {
+        address,
+        resolvedEns
+    };
+
+    var content = string.Join(":", line) + "\n";
+    File.AppendAllText("result.txt", content);
+}
+
+AnsiConsole.MarkupLine("[cyan]Results saved to result.txt[/]");
+Console.ReadKey();
